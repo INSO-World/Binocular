9732edb6f8c5751b553f777417400fc362d64c39
Maximilian Zenz <e1633058@student.tuwien.ac.at>
2024-10-11 13:34:02 +0200

    #261 Frontend Redesign
    - changing web socket to work without a proxy so that the frontend can connect dynamically do multiple backends in the future.

--- binocular-frontend-new/src/redux/socket/socketMiddleware.ts
+++ binocular-frontend-new/src/redux/socket/socketMiddleware.ts
@@ -4,23 +4,38 @@ import { Middleware } from 'redux';
 import SocketFactory, { SocketInterface } from './SocketFactory.ts';
 import { setProgress } from '../general/progressReducer.ts';
 import { ProgressType } from '../../types/general/progressType.ts';
+
 enum SocketEvent {
   Action = 'action',
 }
 
 const socketMiddleware: Middleware = (store) => {
   let socket: SocketInterface;
+  let throttling: boolean = false;
 
   return (next) => (action) => {
     // Middleware logic for the `initSocket` action
     if (!socket && typeof window !== 'undefined') {
       // Client-side-only code
       // Create/ Get Socket Socket
-      socket = SocketFactory.create();
+      socket = SocketFactory.create('ws://localhost:48763');
 
       // Handle all price events
       socket.socket.on(SocketEvent.Action, (progress: ProgressType) => {
-        store.dispatch(setProgress(progress));
+        /*
+         * Progress update needs to be throttled
+         * because there are to many progress updates from the backend
+         * which would cause huge performance issues in the frontend
+         */
+
+        if (!throttling) {
+          throttling = true;
+
+          store.dispatch(setProgress(progress));
+          setTimeout(() => {
+            throttling = false;
+          }, 2000);
+        }
       });
     }
     next(action);
