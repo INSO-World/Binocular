c1f5e9b6c7094bc549c3402013f4c04c5825f4b4
martin <martin.weber.at@gmail.com>
2025-01-23 14:03:19 +0100

    Added SunburstData interface, changed scaling of the chart with and height, added converter for jacoco data.

--- binocular-frontend-new/src/plugins/visualizationPlugins/jacoco/src/chart/chart.tsx
+++ binocular-frontend-new/src/plugins/visualizationPlugins/jacoco/src/chart/chart.tsx
@@ -1,14 +1,35 @@
 import { createRef, useEffect, useState } from 'react';
 import { throttle } from 'throttle-debounce';
 import { SunburstChart } from './sunburstChart.tsx';
+import { convertJacocoDataToSunburstData } from '../utilities/dataConverter.ts';
+
+export interface Counter {
+  missed?: number;
+  covered?: number;
+}
+
+export interface Counters {
+  INSTRUCTION?: Counter;
+  LINE?: Counter;
+  COMPLEXITY?: Counter;
+  METHOD?: Counter;
+  CLASS?: Counter;
+}
+
+export interface SunburstData {
+  name: string;
+  counters?: Counters[];
+  children?: SunburstData[];
+}
 
 function Chart() {
   const chartContainerRef = createRef<HTMLDivElement>();
 
-  const [chartWidth, setChartWidth] = useState(100);
-  const [chartHeight, setChartHeight] = useState(100);
+  const [chartWidth, setChartWidth] = useState(600);
+  const [chartHeight, setChartHeight] = useState(600);
+  const [chartData, setChartData] = useState<SunburstData>({ children: [], counters: [], name: '' });
   const throttledResize = throttle(
-    1000,
+    200,
     () => {
       if (!chartContainerRef.current) return;
       if (chartContainerRef.current?.offsetWidth !== chartWidth) {
@@ -30,10 +51,15 @@ function Chart() {
     return () => resizeObserver.disconnect();
   }, [chartContainerRef, chartHeight, chartWidth]);
 
+  useEffect(() => {
+    const data: SunburstData = convertJacocoDataToSunburstData();
+    setChartData(data);
+  }, []);
+
   return (
     <>
       <div className={'w-full h-full'} ref={chartContainerRef}>
-        <SunburstChart width={chartWidth} height={chartHeight} />
+        <SunburstChart width={chartWidth} height={chartHeight} data={chartData} />
       </div>
     </>
   );
