97110369fcb9bf49aa30a6141f663f026f1c0b7c
martin <martin.weber.at@gmail.com>
2025-01-25 16:14:33 +0100

    Created and registered new visualization plugin jacoco.

---
+++ binocular-frontend-new/src/plugins/visualizationPlugins/jacoco/src/index.tsx
@@ -0,0 +1,29 @@
+import Chart from './chart/chart.tsx';
+import PreviewImage from '../assets/thumbnail.svg';
+import Settings, { SettingsType } from './settings/settings.tsx';
+import { VisualizationPlugin } from '../../../interfaces/visualizationPlugin.ts';
+import Reducer from './reducer';
+import Saga from './saga';
+import Help from './help/help.tsx';
+
+const Jacoco: VisualizationPlugin<SettingsType> = {
+  name: 'Jacoco',
+  chartComponent: Chart,
+  settingsComponent: Settings,
+  helpComponent: Help,
+  defaultSettings: {},
+  export: {
+    getSVGData: () => '<svg></svg>',
+  },
+  capabilities: {
+    popoutOnly: true,
+    export: true,
+  },
+  images: {
+    thumbnail: PreviewImage,
+  },
+  reducer: Reducer,
+  saga: Saga,
+};
+
+export default Jacoco;
