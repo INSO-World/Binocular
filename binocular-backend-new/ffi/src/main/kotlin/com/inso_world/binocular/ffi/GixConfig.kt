ef555725b0e237ea53621160c4b932c5a5c60a53
Manuel Stöger <manuel.stoeger@inso-world.com>
2026-03-06 09:49:51 +0100

    #363: Added missing `GixConfig` class

--- binocular-backend-new/ffi/src/main/kotlin/com/inso_world/binocular/ffi/GixConfig.kt
+++ binocular-backend-new/ffi/src/main/kotlin/com/inso_world/binocular/ffi/GixConfig.kt
@@ -4,11 +4,4 @@ import com.inso_world.binocular.core.BinocularConfig
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-internal open class FfiConfig : BinocularConfig() {
-    lateinit var gix: GixConfig
-}
-
-class GixConfig(
-    val skipMerges: Boolean,
-    val useMailmap: Boolean
-)
+internal open class GixConfig : BinocularConfig()
