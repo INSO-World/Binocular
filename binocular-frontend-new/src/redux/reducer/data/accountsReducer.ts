3c30f0d2f9ee13316295d970640218a7543b1ce3
Megalokom <96736520+Megalokom@users.noreply.github.com>
2026-03-12 19:16:46 +0100

    #411 update reducers to no longer directly edit PayloadActions

--- binocular-frontend-new/src/redux/reducer/data/accountsReducer.ts
+++ binocular-frontend-new/src/redux/reducer/data/accountsReducer.ts
@@ -1,6 +1,7 @@
 import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
 import Config from '../../../config.ts';
 import type { AccountType } from '../../../types/data/accountType.ts';
+import { cloneDeep } from 'lodash';
 
 export interface AccountsInitialState {
   accountLists: { [id: number]: AccountType };
@@ -36,7 +37,8 @@ export const accountsSlice = createSlice({
         // add new accounts that are not in the list
         action.payload.accounts.forEach((account) => {
           if (!accountList.find((a: AccountType) => a.id === account.id)) {
-            account.localId = accountList.length + 1;
+            const newAccount = cloneDeep(account);
+            newAccount.localId = accountList.length + 1;
             accountList.push(account);
           }
         });
