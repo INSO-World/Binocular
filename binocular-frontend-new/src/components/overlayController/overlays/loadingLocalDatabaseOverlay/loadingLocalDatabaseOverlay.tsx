1251d321f4556961bd19ba094329ad1c12016ee9
Megalokom <96736520+Megalokom@users.noreply.github.com>
2026-03-12 21:49:54 +0100

    #411 fix uploading info bug

--- binocular-frontend-new/src/components/overlayController/overlays/loadingLocalDatabaseOverlay/loadingLocalDatabaseOverlay.tsx
+++ binocular-frontend-new/src/components/overlayController/overlays/loadingLocalDatabaseOverlay/loadingLocalDatabaseOverlay.tsx
@@ -4,14 +4,18 @@ import { LocalDatabaseLoadingState } from '../../../../redux/reducer/settings/se
 
 function LoadingLocalDatabaseOverlay() {
   const localDatabaseLoadingState = useSelector((state: RootState) => state.settings.localDatabaseLoadingState);
+  const localDatabaseLoadingMessage = useSelector((state: RootState) => state.settings.localDatabaseLoadingMessage);
   return (
     <>
       {localDatabaseLoadingState === LocalDatabaseLoadingState.loading && (
         <dialog id="my_modal_1" className="modal" open={true}>
           <div className="modal-box">
-            <h3 className="font-bold text-lg">Loading Local Database</h3>
+            <h3 className="font-bold text-lg">Loading Local Database <span className="loading loading-spinner loading-lg text-accent"></span></h3>
             <p className="py-4">
-              <progress className="progress progress-accent w-full"></progress>
+              <progress className="progress progress-accent w-full"
+              value={localDatabaseLoadingMessage.split('/')[0]}
+              max={localDatabaseLoadingMessage.includes('/') ? parseInt(localDatabaseLoadingMessage.split('/')[1]) : 0}></progress>
+              <span>{localDatabaseLoadingMessage}</span>
             </p>
           </div>
         </dialog>
