cbb841a77293f0d453f25eba3589ddc10abb5b39
Bastian Ferch <bastian.ferch@gmail.com>
2026-04-02 15:21:05 +0200

    #437 (New Frontend) Add test framework for unit and ui tests - fix BE undefined creator of issue lead to exception - fix naming of dataplugin contained only id - added utils tests

--- binocular-frontend-new/vitest.config.ts
+++ binocular-frontend-new/vitest.config.ts
@@ -10,14 +10,19 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       reporter: ['text', 'html'],
-      include: ['src/plugins/visualizationPlugins/**/*.{ts,tsx}', 'src/components/**/*.{ts,tsx}', 'src/redux/reducer/**/*.ts'],
+      include: [
+        'src/plugins/visualizationPlugins/**/*.{ts,tsx}',
+        'src/components/**/*.{ts,tsx}',
+        'src/redux/reducer/**/*.ts',
+        'src/redux/middleware/**/*.ts',
+        'src/utils/*.{ts,tsx}',
+      ],
       exclude: [
         'src/test/**',
         'src/plugins/visualizationPlugins/example',
         'src/plugins/visualizationPlugins/**/index.{ts,tsx}',
         'src/plugins/visualizationPlugins/**/help.tsx',
-        'src/plugins/visualizationPlugins/**/src/chart/*.tsx',
-        'src/plugins/visualizationPlugins/**/src/chart/components/**',
+        'src/plugins/visualizationPlugins/**/src/chart/*.tsx', // could be changed to only Chart.tsx
         'src/plugins/visualizationPlugins/**/src/settings/**',
         'src/plugins/visualizationPlugins/**/src/saga/**',
         'src/components/tabMenu/tabMenuContent/**',
