880d9202a5238c9a4d7f0b4540fb8319a808468b
Manuel Stöger <manuel.stoeger@inso-world.com>
2026-05-29 15:44:43 +0200

    #398: Working tests for web (not yet green)

--- binocular-backend-new/infrastructure-arangodb/src/test/kotlin/com/inso_world/binocular/infrastructure/arangodb/Extension.kt
+++ binocular-backend-new/infrastructure-arangodb/src/test/kotlin/com/inso_world/binocular/infrastructure/arangodb/Extension.kt
@@ -14,12 +14,24 @@ import com.inso_world.binocular.infrastructure.arangodb.service.NoteInfrastructu
 import com.inso_world.binocular.infrastructure.arangodb.service.ProjectInfrastructurePortImpl
 import com.inso_world.binocular.infrastructure.arangodb.service.RepositoryInfrastructurePortImpl
 import com.inso_world.binocular.infrastructure.arangodb.service.UserInfrastructurePortImpl
+import org.springframework.aop.framework.Advised
 import org.springframework.transaction.annotation.Transactional
 import java.io.Serializable
 
+/**
+ * Deletes all entities stored by this port.
+ *
+ * Unwraps a Spring AOP proxy (if the receiver is a proxy) before accessing
+ * [AbstractInfrastructurePort.dao], because CGLIB proxies created by
+ * `@MappingSession` skip constructor/init — the proxy's copy of the `lateinit`
+ * field is never assigned on the proxy instance.
+ */
+@Suppress("UNCHECKED_CAST")
 @Transactional
 internal fun <T : Any, I : Serializable> AbstractInfrastructurePort<T, I>.deleteAllEntities() {
-    this.dao.deleteAll()
+    val target = (this as? Advised)?.targetSource?.target as? AbstractInfrastructurePort<T, I>
+        ?: this
+    target.dao.deleteAll()
 }
 
 // Extension functions for specific implementation classes
